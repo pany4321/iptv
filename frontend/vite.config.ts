ae0fd96f05cd02c08c62b49639aefed49af23946
pany4321 <panyuxun@126.com>
2025-10-18 00:08:45 +0800

    第三次提交尝试，继续学习理解

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/ 这是注释
 export default defineConfig({
   plugins: [react()],
 })
